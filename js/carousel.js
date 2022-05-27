cd4bc5fc0759cd1ba9fe36d6431a4cc92d45bc16
wojciechkibitlewski <wojtek.kibitlewski.katowice@gmail.com>
2022-05-27 13:10:30 +0200

    carousel - dodanie przewijania strzałkami

--- js/carousel.js
+++ js/carousel.js
@@ -2,49 +2,65 @@ const swiperSlide = document.querySelectorAll(".swiper-slide");
 const swiperDot = document.querySelectorAll(".fa-circle");
 const swiperAngelLeft = document.querySelector(".fa-angle-left");
 const swiperAngelRight = document.querySelector(".fa-angle-right");
-
+const swiperContainer = document.querySelector(".swiper-container");
 
 const hiddenSlide = (e) => {
-    swiperSlide.forEach((e) => {
-      e.classList.remove("active");
-    });
+  swiperSlide.forEach((e) => {
+    e.classList.remove("active");
+  });
 };
 
 const hiddenDot = (e) => {
-    swiperDot.forEach((e) => {
-        e.classList.remove("light");
-        e.classList.add("dark");
-    });
+  swiperDot.forEach((e) => {
+    e.classList.remove("light");
+    e.classList.add("dark");
+  });
 };
 
-swiperAngelLeft.addEventListener("click", (e) => {
-    console.log("w lewo");
-});
-swiperAngelRight.addEventListener("click", (e) => {
-    let swiperSlideActive = document.querySelector(".swiper-slide.active");
-    let swiperDotActive = document.querySelector(".fa-circle.light");
-    
-    hiddenSlide();
-    swiperSlideActive.nextElementSibling.classList.add("active");
-    hiddenDot();
-    swiperDotActive.nextElementSibling.classList.add("light");
-    console.log("w prawo",swiperSlideActive);
-});
-
-
-let i = 0 
+let i = 0;
 
 function increment() {
-    
-    if (i == swiperSlide.length) { i = 0};
-    hiddenSlide();
-    hiddenDot();
-    swiperSlide[i].classList.add("active");
-    swiperDot[i].classList.add("light");
-    //console.log(swiperSlide[i]);
-    //console.log(swiperDot[i]);
-    //console.log(i)
-    i++;
+  if (i == swiperSlide.length) {
+    i = 0;
+  }
+  hiddenSlide();
+  hiddenDot();
+  swiperSlide[i].classList.add("active");
+  swiperDot[i].classList.add("light");
+
+  i++;
 }
 
-setInterval(increment, 5000)
+swiperAngelLeft.addEventListener("click", (e) => {
+  let swiperSlideActive = document.querySelector(".swiper-slide.active");
+  let swiperDotActive = document.querySelector(".fa-circle.light");
+
+  if (i == 0) {
+    i = swiperSlide.length;
+  }
+  i--;
+
+  hiddenSlide();
+  hiddenDot();
+  swiperSlide[i].classList.add("active");
+  swiperDot[i].classList.add("light");
+});
+
+swiperAngelRight.addEventListener("click", (e) => {
+  let swiperSlideActive = document.querySelector(".swiper-slide.active");
+  let swiperDotActive = document.querySelector(".fa-circle.light");
+  i++;
+  if (i == swiperSlide.length) {
+    i = 0;
+  }
+
+  hiddenSlide();
+  hiddenDot();
+  swiperSlide[i].classList.add("active");
+  swiperDot[i].classList.add("light");
+});
+
+setInterval(increment, 4000);
+
+
+
